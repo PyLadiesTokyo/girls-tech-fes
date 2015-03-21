1220a47b26611f1fedba0ba9c9307d5aa0c912dc
Ai Makabi <a.macbee@gmail.com>
2015-03-21 17:08:24 +0900

    3to2を利用してPython2系環境に対応

--- src/chatdemo.py
+++ src/chatdemo.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
 # Copyright 2009 Facebook
 #
@@ -25,27 +25,27 @@ import uuid
 
 from tornado.options import define, options
 
-define("port", default=8888, help="run on the given port", type=int)
-define("debug", default=False, help="run in debug mode")
+define(u"port", default=8888, help=u"run on the given port", type=int)
+define(u"debug", default=False, help=u"run in debug mode")
 
 
 class Application(tornado.web.Application):
     def __init__(self):
-        #URLの指定
+        # URLの指定
         handlers = [
-            (r"/", MainHandler),
-            (r"/name/inputname", InputNameHandler),
-            (r"/name/signup", SignUpHandler),
-            (r"/name/signout", SignOutHandler),
-            (r"/chatsocket", ChatSocketHandler),
+            (ur"/", MainHandler),
+            (ur"/name/inputname", InputNameHandler),
+            (ur"/name/signup", SignUpHandler),
+            (ur"/name/signout", SignOutHandler),
+            (ur"/chatsocket", ChatSocketHandler),
         ]
         settings = dict(
-            cookie_secret="__pyladies_girls_tech_fes_demo_cookie__",
-            template_path=os.path.join(os.path.dirname(__file__), "templates"),
-            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            cookie_secret=u"__pyladies_girls_tech_fes_demo_cookie__",
+            template_path=os.path.join(os.path.dirname(__file__), u"templates"),
+            static_path=os.path.join(os.path.dirname(__file__), u"static"),
 
-            login_url="/name/inputname",
-            autoescape="xhtml_escape",
+            login_url=u"/name/inputname",
+            autoescape=u"xhtml_escape",
             xsrf_cookies=True,
             debug=options.debug,
         )
@@ -54,7 +54,7 @@ class Application(tornado.web.Application):
 
 class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
-        username = self.get_secure_cookie("chatdemo_user")
+        username = self.get_secure_cookie(u"chatdemo_user")
 
         # cookieが値を返すとパスできる
         # 何もないとlogin_urlへリダイレクトされる → login_urlの指定がなければloginなしのシステムとなる
@@ -66,7 +66,7 @@ class BaseHandler(tornado.web.RequestHandler):
 class MainHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
-        self.render("index.html", messages=ChatSocketHandler.cache)
+        self.render(u"index.html", messages=ChatSocketHandler.cache)
 
 
 class ChatSocketHandler(tornado.websocket.WebSocketHandler):
@@ -92,27 +92,27 @@ class ChatSocketHandler(tornado.websocket.WebSocketHandler):
 
     @classmethod
     def send_updates(cls, chat):
-        logging.info("sending message to %d waiters", len(cls.waiters))
-        print(chat)
+        logging.info(u"sending message to %d waiters", len(cls.waiters))
+        print chat
         for waiter in cls.waiters:
             try:
                 waiter.write_message(chat)
             except:
-                logging.error("Error sending message", exc_info=True)
+                logging.error(u"Error sending message", exc_info=True)
 
     # 表示メッセージの整形
     def on_message(self, message):
-        logging.info("got message %r", message)
+        logging.info(u"got message %r", message)
         parsed = tornado.escape.json_decode(message)
-        username = self.get_secure_cookie("chatdemo_user")
+        username = self.get_secure_cookie(u"chatdemo_user")
 
         chat = {
-            "id": str(uuid.uuid4()),
-            "from": str(username, encoding='utf-8'),  # html表示時文字化け対策実施
-            "body": parsed["body"],
+            u"id": unicode(uuid.uuid4()),
+            u"from": unicode(username, encoding=u'utf-8'),  # html表示時文字化け対策実施
+            u"body": parsed[u"body"],
             }
-        chat["html"] = tornado.escape.to_basestring(
-            self.render_string("message.html", message=chat))
+        chat[u"html"] = tornado.escape.to_basestring(
+            self.render_string(u"message.html", message=chat))
 
         ChatSocketHandler.update_cache(chat)
         ChatSocketHandler.send_updates(chat)
@@ -121,25 +121,25 @@ class ChatSocketHandler(tornado.websocket.WebSocketHandler):
 # ログインの代わりに名前の入力してもらう
 class InputNameHandler(BaseHandler):
     def get(self):
-        self.render("inputname.html")
+        self.render(u"inputname.html")
 
 # 入力した名前をCookieにセット。チャットページで使えるようにする。
 class SignUpHandler(BaseHandler):
     def get(self):
-        self.render("login.html")
+        self.render(u"login.html")
 
     # <form action="/name/setname" method="post"> の記述により
     # inputname.htmlから最初に呼ばれるdefはこちら。
     def post(self):
-        logging.debug("xsrf_cookie:" + self.get_argument("_xsrf", None))
+        logging.debug(u"xsrf_cookie:" + self.get_argument(u"_xsrf", None))
 
         self.check_xsrf_cookie()
-        username = self.get_argument("username")
-        logging.debug('サインアップメソッドで %s の名前を受け取りました' % username)
+        username = self.get_argument(u"username")
+        logging.debug(u'サインアップメソッドで {0} の名前を受け取りました'.format(username))
 
         if username:
-            self.set_secure_cookie("chatdemo_user", tornado.escape.utf8(username))
-            self.redirect("/")
+            self.set_secure_cookie(u"chatdemo_user", tornado.escape.utf8(username))
+            self.redirect(u"/")
         else:
             self.write_error(403)
 
@@ -148,16 +148,19 @@ class SignUpHandler(BaseHandler):
 # 本来今回のworkshopでは不要な処理です。。。
 class SignOutHandler(BaseHandler):
     def get(self):
-        self.clear_cookie("chatdemo_user")
-        self.write("登録していたユーザ名を消去しました。もう一度はじめからやり直しをお願いします。")
+        self.clear_cookie(u"chatdemo_user")
+        self.write(u"登録していたユーザ名を消去しました。もう一度はじめからやり直しをお願いします。")
 
 
 def main():
     tornado.options.parse_command_line()
     app = Application()
     app.listen(options.port)
+    print u"サーバを起動します。"
+    print u"http://localhost:" + str(options.port) + u" にアクセスしてください"
+    print u"止めるときは Ctrl + c を押してください"
     tornado.ioloop.IOLoop.instance().start()
 
 
-if __name__ == "__main__":
+if __name__ == u"__main__":
     main()
